9dc452a62ccf7b57d1ed200ff3a45dd23d93d7a4
florian.laine <florian.laine@epita.fr>
2024-04-18 02:26:52 +0200

    feat: working cube

--- src/object/transform.cc
+++ src/object/transform.cc
@@ -7,15 +7,15 @@
 
 mat4 Transform::getMatrix() {
     glm::mat4 m = glm::translate(glm::mat4(1.0f), position);
-    for (int i = 0; i < 4; i++)
-    {
-        for (int j = 0; j < 4; j++)
-        {
-            std::cout << m[i][j] << " ";
-        }
-        std::cout << "" << '\n';
+    /* for (int i = 0; i < 4; i++) */
+    /* { */
+    /*     for (int j = 0; j < 4; j++) */
+    /*     { */
+    /*         std::cout << m[i][j] << " "; */
+    /*     } */
+    /*     std::cout << "" << '\n'; */
             
-    }
+    /* } */
     return m;
 }
 
