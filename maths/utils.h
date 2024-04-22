23b013f9ad9c5b5cb56ec6e0e44ddc5058b3c12b
florian.laine <florian.laine@epita.fr>
2024-04-22 05:47:23 +0200

    fix: cleanup dir

--- maths/utils.h
+++
@@ -1,7 +0,0 @@
-#ifndef UTILS_H
-#define UTILS_H
-#define M_PI 3.14159265358979323846
-#define RADIAN(X) ((X) * M_PI / 180.0)
-#define DEGREE(X) ((X) * 180.0 / M_PI)
-#define UNUSED(X) ((void)X)
-#endif /* !UTILS_H */
